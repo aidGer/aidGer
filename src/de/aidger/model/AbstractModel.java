15a1ce2ab8c0dff9a8f3a4ba88ab0375e5a0413f
Philipp Gildein <rmbl@openspeak-project.org>
2010-04-15 17:03:24 +0200

    Add comments and correct abstract model and dbwrapper

--- src/de/aidger/model/AbstractModel.java
+++ src/de/aidger/model/AbstractModel.java
@@ -10,28 +10,51 @@ package de.aidger.model;
 public class AbstractModel {
 
     /**
-     * Private constructor so noone can construct AbstractModel directly
+     * Private constructor so no one can construct AbstractModel directly
      */
     private AbstractModel() {
 
     }
     
-    public static AbstractModel getAll() {
+    /**
+     * Get all models from the database
+     * 
+     * @return An array containing all found models or null
+     */
+    public static AbstractModel[] getAll() {
 	return null;
     }
     
+    /**
+     * Get a specific model by specifying its unique id
+     * 
+     * @param id The unique id of the model
+     * @return The model if one was found or null
+     */
     public static AbstractModel getById(int id) {
 	return null;
     }
     
+    /**
+     * Get a specific model by specifying a set of keys
+     * 
+     * @param o The set of keys specific to this model
+     * @return The model if one was found or null
+     */
     public static AbstractModel getByKeys(Object... o) {
 	return null;
     }
     
+    /**
+     * Save the current model to the database
+     */
     public void save() {
 	
     }
     
+    /**
+     * Remove the current model from the database
+     */
     public void remove() {
 	
     }
--- src/de/aidger/model/DataBaseWrapper.java
+++ src/de/aidger/model/DataBaseWrapper.java
@@ -12,22 +12,56 @@ import java.util.*;
  */
 public final class DataBaseWrapper {
     
-    public static <T extends AbstractModel> List<T> getAll() {
+    // TODO: Needs to be a Singleton
+
+    /**
+     * Get all models of the specified type
+     * 
+     * @param <T> The type of the model
+     * @return A list containing all models or null
+     */
+    public <T extends AbstractModel> List<T> getAll() {
 	return new Vector<T>();
     }
     
-    public static <T extends AbstractModel> T getById(int id) {
+    /**
+     * Get a specific model by specifying its unique id
+     * 
+     * @param <T> The type of the model
+     * @param id The unique id of the model
+     * @return The model if one was found or null
+     */
+    public <T extends AbstractModel> T getById(int id) {
 	return null;
     }
     
-    public static <T extends AbstractModel> T getByKeys(Object... o) {
+    /**
+     * Get a specific model by specifying a set of keys
+     * 
+     * @param <T> The type of the model
+     * @param o The set of keys specific to this model
+     * @return The model if one was found or null
+     */
+    public <T extends AbstractModel> T getByKeys(Object... o) {
 	return null;
     }
     
+    /**
+     * Save the model to the database
+     * 
+     * @param <T> The type of the model
+     * @param model The model to save
+     */
     public <T extends AbstractModel> void save(T model) {
 	
     }
     
+    /**
+     * Remove the model from the database
+     * 
+     * @param <T> The type of the model
+     * @param model The model to remove
+     */
     public <T extends AbstractModel> void remove(T model) {
 	
     }
