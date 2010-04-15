7c977c47a32f96ed1bb74286966f036fd2b87679
Philipp Gildein <rmbl@pgserv.net>
2010-04-15 13:35:53 +0200

    Add Application class

---
+++ src/de/aidger/controller/Application.java
@@ -0,0 +1,13 @@
+package de.aidger.controller;
+
+public final class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
