7e49936531bb783ca0f4475fab8f65559225c70d
Philipp Gildein <rmbl@openspeak-project.org>
2010-04-28 20:43:35 +0200

    Implement all method stubs and add javadoc comments for Activity class.

--- src/de/aidger/model/models/Activity.java
+++ src/de/aidger/model/models/Activity.java
@@ -5,131 +5,273 @@ import java.util.Date;
 import de.aidger.model.AbstractModel;
 import de.unistuttgart.iste.se.adohive.model.IActivity;
 
+/**
+ * Represents a single entry in the activity column of the database.
+ * Contains functions to retrieve and change the data in the database.
+ * 
+ * @author aidGer Team
+ *
+ */
 public class Activity extends AbstractModel implements IActivity {
 	
+	/**
+	 * The unique id of the element in the database.
+	 */
+	private int id;
+	
+	/**
+	 * References the assistant by its unique id.
+	 */
+	private int assistantId;
+	
+	/**
+	 * References the course by its unique id.
+	 */
+	private int courseId;
+	
+	/**
+	 * The date on which this activity took place.
+	 */
+	private Date date;
+	
+	/**
+	 * The sender of the activity.
+	 */
+	private String sender;
+	
+	/**
+	 * The type of document referenced by this activity.
+	 */
+	private String documentType;
+	
+	/**
+	 * The type of the activity.
+	 */
+	private String type;
+	
+	/**
+	 * The processor of the activity.
+	 */
+	private String processor;
+	
+	/**
+	 * The contents of the activity.
+	 */
+	private String content;
+	
+	/**
+	 * Remarks for the activity.
+	 */
+	private String remark;
+	
+	/** 
+	 * Clone the current activity
+	 */
 	public Activity clone()
 	{
 		return new Activity();
 	}
 
+	/**
+	 * Returns the id of the referenced assistant.
+	 * 
+	 * @return The id of the referenced assistant
+	 */
 	@Override
-	public int getAssistantId() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getAssistantId() {		
+		return assistantId;
 	}
 
+	/**
+	 * Returns the contents of the activity.
+	 * 
+	 * @return The contents of the activity
+	 */
 	@Override
-	public String getContent() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getContent() {		
+		return content;
 	}
 
+	/**
+	 * Returns the id of the referenced course.
+	 * 
+	 * @return The id of the referenced course.
+	 */
 	@Override
-	public int getCourseId() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getCourseId() {		
+		return courseId;
 	}
 
+	/**
+	 * Returns the date of the activity.
+	 * 
+	 * @return The date of the activity.
+	 */
 	@Override
-	public Date getDate() {
-		// TODO Auto-generated method stub
-		return null;
+	public Date getDate() {		
+		return date;
 	}
 
+	/**
+	 * Returns the type of document.
+	 * 
+	 * @return The type of document
+	 */
 	@Override
-	public String getDocumentType() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getDocumentType() {		
+		return documentType;
 	}
 
+	/**
+	 * Returns the processor of the activity.
+	 * 
+	 * @return The processor of the activity.
+	 */
 	@Override
 	public String getProcessor() {
-		// TODO Auto-generated method stub
-		return null;
+		return processor;
 	}
 
+	/**
+	 * Returns remarks to the activity.
+	 * 
+	 * @return Remarks to the activity
+	 */
 	@Override
 	public String getRemark() {
-		// TODO Auto-generated method stub
-		return null;
+		return remark;
 	}
 
+	/**
+	 * Returns the sender of the activity.
+	 * 
+	 * @return The sender of the activity.
+	 */
 	@Override
 	public String getSender() {
-		// TODO Auto-generated method stub
-		return null;
+		return sender;
 	}
 
+	/**
+	 * Returns the type of the activity.
+	 * 
+	 * @return The type of the activity
+	 */
 	@Override
 	public String getType() {
-		// TODO Auto-generated method stub
-		return null;
+		return type;
 	}
 
+	/**
+	 * Set the id of the assistant referenced by this activity.
+	 * 
+	 * @param id The id of the assistant
+	 */
 	@Override
-	public void setAssistantId(int arg0) {
-		// TODO Auto-generated method stub
-
+	public void setAssistantId(int id) {
+		assistantId = id;
 	}
 
+	/**
+	 * Set the contents of the activity.
+	 * 
+	 * @param cont The contents of the activity
+	 */
 	@Override
-	public void setContent(String arg0) {
-		// TODO Auto-generated method stub
-
+	public void setContent(String cont) {
+		content = cont;
 	}
 
+	/**
+	 * Set the id of the course referenced by this activity.
+	 * 
+	 * @param id The id of the course
+	 */
 	@Override
-	public void setCourseId(int arg0) {
-		// TODO Auto-generated method stub
-
+	public void setCourseId(int id) {
+		courseId = id;
 	}
 
+	/**
+	 * Set the date of the activity.
+	 * 
+	 * @param dt The date of the activity
+	 */
 	@Override
-	public void setDate(Date arg0) {
-		// TODO Auto-generated method stub
-
+	public void setDate(Date dt) {
+		date = dt;
 	}
 
+	/**
+	 * Set the type of the document
+	 * 
+	 * @param type The type of the document
+	 */
 	@Override
-	public void setDocumentType(String arg0) {
-		// TODO Auto-generated method stub
-
+	public void setDocumentType(String type) {
+		documentType = type;
 	}
 
+	/**
+	 * Set the processor of the activity.
+	 * 
+	 * @param proc The processor of the activity
+	 */
 	@Override
-	public void setProcessor(String arg0) {
-		// TODO Auto-generated method stub
-
+	public void setProcessor(String proc) {
+		processor = proc;
 	}
 
+	/**
+	 * Set remarks to the activity.
+	 * 
+	 * @param rem Remarks to the activity
+	 */
 	@Override
-	public void setRemark(String arg0) {
-		// TODO Auto-generated method stub
-
+	public void setRemark(String rem) {
+		remark = rem;
 	}
 
+	/**
+	 * Set the sender of the activity.
+	 * 
+	 * @param send The sender of the activity
+	 */
 	@Override
-	public void setSender(String arg0) {
-		// TODO Auto-generated method stub
-
+	public void setSender(String send) {
+		sender = send;
 	}
 
+	/**
+	 * Set the type of the activity.
+	 * 
+	 * @param typec The type of the activity
+	 */
 	@Override
-	public void setType(String arg0) {
-		// TODO Auto-generated method stub
-
+	public void setType(String typec) {
+		type = typec;
 	}
 
+	/**
+	 * Returns the unique id of the activity.
+	 * 
+	 * @return The unique id of the activity
+	 */
 	@Override
-	public int getId() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getId() {		
+		return id;
 	}
 
+	/**
+	 * Set the unique id of the activity
+	 * 
+	 * <b>!!! THIS IS ONLY FOR INTERNAL ADOHIVE USAGE !!!</b>
+	 * 
+	 * @param id The unique id of the activity
+	 */
 	@Override
-	public void setId(int arg0) {
-		// TODO Auto-generated method stub
-
+	public void setId(int id) {
+		this.id = id;
 	}
 
 }
