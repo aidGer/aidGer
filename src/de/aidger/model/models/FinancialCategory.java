d228f32c46369a8b1b0808f2f24dd80d5b59892d
Philipp Gildein <rmbl@openspeak-project.org>
2010-04-28 22:04:12 +0200

    Implement and comment FinancialCategory

--- src/de/aidger/model/models/FinancialCategory.java
+++ src/de/aidger/model/models/FinancialCategory.java
@@ -3,72 +3,149 @@ package de.aidger.model.models;
 import de.aidger.model.AbstractModel;
 import de.unistuttgart.iste.se.adohive.model.IFinancialCategory;
 
+/**
+ * Represents a single entry in the financial category column of the database.
+ * Contains functions to retrieve and change the data in the database.
+ *
+ * @author aidGer Team
+ */
 public class FinancialCategory extends AbstractModel implements
 		IFinancialCategory {
 
+	/**
+	 * The unique id of the category.
+	 */
+	private int id;
+
+	/**
+	 * The name of the category.
+	 */
+	private String name;
+
+	/**
+	 * The budget costs of the category.
+	 */
+	private int[] budgetCosts;
+
+	/**
+	 * The fonds of the category.
+	 */
+	private int[] fonds;
+
+	/**
+	 * The year the category is valid.
+	 */
+	private short year;
+
+	/**
+	 * Clone the current category.
+	 */
+	@Override
 	public FinancialCategory clone()
 	{
 		return new FinancialCategory();
 	}
 
+	/**
+	 * Get the budget costs of the category.
+	 *
+	 * @return The budget costs of the category
+	 */
 	@Override
 	public int[] getBugdetCosts() {
-		// TODO Auto-generated method stub
-		return null;
+		return budgetCosts;
 	}
 
+	/**
+	 * Get the fonds of the category.
+	 *
+	 * @return The fonds of the category
+	 */
 	@Override
 	public int[] getFonds() {
-		// TODO Auto-generated method stub
-		return null;
+		return fonds;
 	}
 
+	/**
+	 * Get the name of the category.
+	 *
+	 * @return The name of the category
+	 */
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
+	/**
+	 * Get the year the category is valid.
+	 *
+	 * @return The year the category is valid
+	 */
 	@Override
 	public short getYear() {
-		// TODO Auto-generated method stub
-		return 0;
+		return year;
 	}
 
+	/**
+	 * Set the budget costs of the category.
+	 *
+	 * @param costs The budget costs of the category
+	 */
 	@Override
-	public void setBugdetCosts(int[] arg0) {
-		// TODO Auto-generated method stub
-
+	public void setBugdetCosts(int[] costs) {
+		budgetCosts = costs;
 	}
 
+	/**
+	 * Set the fonds of the category.
+	 *
+	 * @param fonds The fonds of the category
+	 */
 	@Override
-	public void setFonds(int[] arg0) {
-		// TODO Auto-generated method stub
-
+	public void setFonds(int[] fonds) {
+		this.fonds = fonds;
 	}
 
+	/**
+	 * Set the name of the category.
+	 *
+	 * @param name The name of the category
+	 */
 	@Override
-	public void setName(String arg0) {
-		// TODO Auto-generated method stub
-
+	public void setName(String name) {
+		this.name = name;
 	}
 
+	/**
+	 * Set the year the category is valid.
+	 *
+	 * @param year The year the category is valid.
+	 */
 	@Override
-	public void setYear(short arg0) {
-		// TODO Auto-generated method stub
-
+	public void setYear(short year) {
+		this.year = year;
 	}
 
+	/**
+	 * Returns the unique id of the category.
+	 *
+	 * @return The unique id of the category
+	 */
 	@Override
 	public int getId() {
-		// TODO Auto-generated method stub
-		return 0;
+		return id;
 	}
 
+	/**
+	 * Set the unique id of the category.
+	 *
+	 * <b>!!! THIS IS FOR INTERNAL ADOHIVE USAGE ONLY !!!</b>
+	 *
+	 * @param id The unique id of the category
+	 */
 	@Override
-	public void setId(int arg0) {
-		// TODO Auto-generated method stub
-
+	public void setId(int id) {
+		this.id = id;
 	}
 
 }
