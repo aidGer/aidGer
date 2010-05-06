ba1a243019099e6f9627e2b6c2839299cc6f7f06
Philipp Gildein <rmbl@openspeak-project.org>
2010-05-06 16:59:15 +0200

    Add homepage and close button to AboutDialog

--- src/de/aidger/view/AboutDialog.java
+++ src/de/aidger/view/AboutDialog.java
@@ -7,12 +7,17 @@ import java.awt.Frame;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 
+import de.aidger.controller.ActionRegistry;
+import de.aidger.controller.actions.AbortAction;
+
 public class AboutDialog extends JDialog {
 
     private static final long serialVersionUID = 1L;
     private JPanel jContentPane = null;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
 
     /**
      * @param owner
@@ -28,7 +33,7 @@ public class AboutDialog extends JDialog {
      * @return void
      */
     private void initialize() {
-        this.setSize(460, 110);
+        this.setSize(480, 145);
         setTitle(_("About aidGer"));
         setContentPane(getJContentPane());
     }
@@ -43,7 +48,9 @@ public class AboutDialog extends JDialog {
             jContentPane = new JPanel();
             jLabel1 = new javax.swing.JLabel();
             jLabel2 = new javax.swing.JLabel();
-            // TODO: Add link to homepage
+            jButton1 = new javax.swing.JButton(ActionRegistry.getInstance()
+                    .get(AbortAction.class.getName()));
+            jLabel3 = new javax.swing.JLabel();
 
             setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -54,9 +61,14 @@ public class AboutDialog extends JDialog {
             jLabel2
                     .setText(_("Authors: Christian Buchgraber, Philipp Gildein, Philipp Pirrung"));
 
+            jButton1.setText(_("Close"));
+
+            jLabel3.setText(_("Homepage: http://www.aidger.de"));
+
             javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
                     jContentPane);
             jContentPane.setLayout(layout);
+
             layout
                     .setHorizontalGroup(layout
                             .createParallelGroup(
@@ -70,32 +82,57 @@ public class AboutDialog extends JDialog {
                                                             .createParallelGroup(
                                                                     javax.swing.GroupLayout.Alignment.LEADING)
                                                             .addComponent(
-                                                                    jLabel2)
-                                                            .addComponent(
                                                                     jLabel1,
                                                                     javax.swing.GroupLayout.DEFAULT_SIZE,
-                                                                    438,
-                                                                    Short.MAX_VALUE))
+                                                                    459,
+                                                                    Short.MAX_VALUE)
+                                                            .addComponent(
+                                                                    jLabel2)
+                                                            .addGroup(
+                                                                    javax.swing.GroupLayout.Alignment.TRAILING,
+                                                                    layout
+                                                                            .createSequentialGroup()
+                                                                            .addComponent(
+                                                                                    jLabel3)
+                                                                            .addPreferredGap(
+                                                                                    javax.swing.LayoutStyle.ComponentPlacement.RELATED,
+                                                                                    208,
+                                                                                    Short.MAX_VALUE)
+                                                                            .addComponent(
+                                                                                    jButton1)))
                                             .addContainerGap()));
             layout
                     .setVerticalGroup(layout
                             .createParallelGroup(
                                     javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(
-                                    javax.swing.GroupLayout.Alignment.TRAILING,
                                     layout
                                             .createSequentialGroup()
                                             .addContainerGap()
                                             .addComponent(jLabel1)
                                             .addPreferredGap(
-                                                    javax.swing.LayoutStyle.ComponentPlacement.RELATED,
-                                                    23, Short.MAX_VALUE)
-                                            .addComponent(jLabel2)
-                                            .addContainerGap()));
+                                                    javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                            .addComponent(
+                                                    jLabel2,
+                                                    javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                    26,
+                                                    javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addPreferredGap(
+                                                    javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                            .addGroup(
+                                                    layout
+                                                            .createParallelGroup(
+                                                                    javax.swing.GroupLayout.Alignment.LEADING)
+                                                            .addComponent(
+                                                                    jButton1)
+                                                            .addComponent(
+                                                                    jLabel3))
+                                            .addContainerGap(
+                                                    javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                    Short.MAX_VALUE)));
 
             pack();
         }
         return jContentPane;
     }
-
 }
