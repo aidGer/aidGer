fc6441a3f457d7a9fd02d285ba2a093b80ef6eb3
Philly <pille_kof@hotmail.com>
2010-06-14 22:24:58 +0200

    Removed dependencies of creator classes on their viewers. (refs #61)

--- src/de/aidger/utils/reports/BalanceHelper.java
+++ src/de/aidger/utils/reports/BalanceHelper.java
@@ -123,6 +123,77 @@ public class BalanceHelper {
         return filteredTriceCourses;
     }
 
+    public String[] getYearSemesters(int year) {
+        // Lose the first two numbers of the year
+        int semester = year % 100;
+        boolean returnBoolean = false;
+        /*
+         * Contains the year in YYYY form, the previous, current and next
+         * semester in that order.
+         */
+        String[] semesters = new String[4];
+        semesters[0] = "" + year;
+        switch (semester) {
+        /*
+         * If the given year is 2001-2008, (year % 100) will give a single
+         * number below 9. Therefore, the previous, current and next semester
+         * all need a leading 0 added.
+         */
+        case 0:
+            semesters[1] = "WS" + "99" + "00";
+            semesters[2] = "SS" + "00";
+            semesters[3] = "WS" + "00" + "01";
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+            semesters[1] = "WS0" + (semester - 1) + "0" + semester;
+            semesters[2] = "SS0" + semester;
+            semesters[3] = "WS0" + semester + "0" + (semester + 1);
+            break;
+        /*
+         * If the given year is 2009, the previous and current semester will
+         * both be a single number and therefore need a leading 0 added. The
+         * next semester will be 10 and thus needs no adjustments.
+         */
+        case 9:
+            semesters[1] = "WS0" + (semester - 1) + "0" + semester;
+            semesters[2] = "SS0" + semester;
+            semesters[3] = "WS0" + semester + (semester + 1);
+            break;
+        /*
+         * If the given year is 2010, the current and next semesters will be 10
+         * and 11 and therefore don't need a leading 0. The previous semester
+         * will be 9 though.
+         */
+        case 10:
+            semesters[1] = "WS0" + (semester - 1) + semester;
+            semesters[2] = "SS" + semester;
+            semesters[3] = "WS" + semester + (semester + 1);
+            break;
+        case 99:
+            semesters[1] = "WS" + (semester - 1) + semester;
+            semesters[2] = "SS" + semester;
+            semesters[3] = "WS" + semester + "00";
+            break;
+        /*
+         * In all other relevant cases (11 and higher), the semesters can be
+         * used the way (year % 100) returns them.
+         */
+        default:
+            semesters[1] = "WS" + (semester - 1) + semester;
+            semesters[2] = "SS" + semester;
+            semesters[3] = "WS" + semester + (semester + 1);
+            break;
+        }
+        return semesters;
+    }
+
     /**
      * Calculates the data relevant for the balance into a balance course model.
      * Returns that balance course model.
@@ -318,5 +389,29 @@ public class BalanceHelper {
             years.add("");
             return years;
         }
+    }
+
+    /**
+     * Checks if the given semester contains any courses.
+     * 
+     * @param semester
+     *            The semester to check
+     * @return true if the semester contains one or more courses.
+     */
+    public boolean courseExists(String semester, BalanceFilter filters) {
+        List<ICourse> courses = null;
+        try {
+            courses = (new Course()).getAll();
+        } catch (AdoHiveException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        List<ICourse> filteredCourses = this.filterCourses(courses, filters);
+        for (ICourse course : filteredCourses) {
+            if (course.getSemester().equals(semester)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
