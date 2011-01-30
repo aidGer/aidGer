c1c1375435152309071c5107e965632066929d1e
Christian Buchgraber <buchgrcn@studi.informatik.uni-stuttgart.de>
2011-01-30 17:16:25 +0100

    courses are sorted chronologically instead of alphabetically (fixes #287)

--- src/de/aidger/view/models/UICourse.java
+++ src/de/aidger/view/models/UICourse.java
@@ -19,6 +19,9 @@
 
 package de.aidger.view.models;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import de.aidger.model.models.Course;
 import de.aidger.view.tabs.ViewerTab.DataType;
 import de.unistuttgart.iste.se.adohive.model.ICourse;
@@ -28,7 +31,13 @@ import de.unistuttgart.iste.se.adohive.model.ICourse;
  * 
  * @author aidGer Team
  */
-public class UICourse extends Course implements UIModel {
+public class UICourse extends Course implements UIModel, Comparable<UICourse> {
+
+    /**
+     * The pattern for a semester (SSXX, WSXXXX).
+     */
+    private final Pattern patternSemester = Pattern
+        .compile("[SS|WS]([0-9]{2})[0-9]{0,2}");
 
     /**
      * Initializes the Course class.
@@ -69,5 +78,43 @@ public class UICourse extends Course implements UIModel {
     @Override
     public DataType getDataType() {
         return DataType.Course;
+    }
+
+    /**
+     * Returns the year of a given semester.
+     * 
+     * @param semester
+     *            the semester (SSXX, WSXXXX, XXXX)
+     * @return the year in 4 digits
+     */
+    private Integer getYearOfSemester(String semester) {
+        Matcher m = patternSemester.matcher(semester);
+        return m.find() ? Integer.valueOf("20" + m.group(1)) : Integer
+            .valueOf(semester);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
+    @Override
+    public int compareTo(UICourse o) {
+        Integer year = getYearOfSemester(getSemester()), oYear = getYearOfSemester(o
+            .getSemester());
+
+        if (year > oYear) {
+            return 1;
+        } else if (year < oYear) {
+            return -1;
+        } else {
+            int c = getSemester().substring(0, 1).compareTo(
+                o.getSemester().substring(0, 1));
+            if (c == 0) {
+                return getDescription().compareTo(o.getDescription());
+            } else {
+                return c;
+            }
+        }
     }
 }
