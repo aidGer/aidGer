61b6fb788effe9f9e97ed9e8ecd8bce98447ab63
Christian Buchgraber <buchgrcn@studi.informatik.uni-stuttgart.de>
2011-01-31 17:50:24 +0100

    correct bugfix (refs #293)

--- src/de/aidger/view/models/UICourse.java
+++ src/de/aidger/view/models/UICourse.java
@@ -88,12 +88,13 @@ public class UICourse extends Course implements UIModel, Comparable<UICourse> {
      * @return the year in 4 digits
      */
     private Integer getYearOfSemester(String semester) {
-        if (semester != null) {
-            Matcher m = patternSemester.matcher(semester);
-            return m.find() ? Integer.valueOf("20" + m.group(1)) : Integer
-                .valueOf(semester);
+        if (semester == null) {
+            return 0;
         }
-        return 0;
+
+        Matcher m = patternSemester.matcher(semester);
+        return m.find() ? Integer.valueOf("20" + m.group(1)) : Integer
+            .valueOf(semester);
     }
 
     /*
@@ -103,6 +104,16 @@ public class UICourse extends Course implements UIModel, Comparable<UICourse> {
      */
     @Override
     public int compareTo(UICourse o) {
+        int c = 0;
+
+        if (getDescription() != null && o.getDescription() != null) {
+            c = getDescription().compareTo(o.getDescription());
+        }
+
+        if (getSemester() == null || o.getSemester() == null) {
+            return c;
+        }
+
         Integer year = getYearOfSemester(getSemester()), oYear = getYearOfSemester(o
             .getSemester());
 
@@ -111,12 +122,12 @@ public class UICourse extends Course implements UIModel, Comparable<UICourse> {
         } else if (year < oYear) {
             return -1;
         } else {
-            int c = getSemester().substring(0, 1).compareTo(
+            int c_ = getSemester().substring(0, 1).compareTo(
                 o.getSemester().substring(0, 1));
-            if (c == 0) {
-                return getDescription().compareTo(o.getDescription());
-            } else {
+            if (c_ == 0) {
                 return c;
+            } else {
+                return c_;
             }
         }
     }
