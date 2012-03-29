254505d9bae55c690b3c438145311551ac79309e
Philipp Gildein <philipp@gildein.com>
2012-03-29 14:39:42 +0200

    Fix db name

--- tests/de/unistuttgart/iste/se/adohive/controller/mysql/test/MySqlInit.java
+++ tests/de/unistuttgart/iste/se/adohive/controller/mysql/test/MySqlInit.java
@@ -13,6 +13,6 @@ public class MySqlInit {
 
 	public static void init() {
 		AdoHiveController.setDriver("com.mysql.jdbc.Driver");
-		AdoHiveController.setConnectionString("jdbc:mysql://localhost:3306/aidger-test?user=root");
+		AdoHiveController.setConnectionString("jdbc:mysql://localhost:3306/aidgertest?user=root");
 	}
 }
