696d5706324beac27faf3f6fa1f4f84f5d7cd97f
Philipp Gildein <philipp@gildein.com>
2012-06-13 17:20:21 +0200

    Another schema change for fcs

--- schemas/alter_mysql_1.5.sql
+++ schemas/alter_mysql_1.5.sql
@@ -7,4 +7,8 @@ ALTER TABLE `Stundenlohn` ADD `ID` int(11) NOT NULL AUTO_INCREMENT,
     ADD PRIMARY KEY (`ID`),
     ADD UNIQUE KEY `QualJahrMonat` (`Qualifikation`, `Jahr`, `Monat`);
 
-ALTER TABLE `Finanzkategorie` DROP PRIMARY KEY, ADD PRIMARY KEY (`ID`);
+ALTER TABLE `Finanzkategorie` DROP PRIMARY KEY,
+    CHANGE `ID` `Gruppe` int(11) NOT NULL,
+    AUTO_INCREMENT = 1,
+    ADD `ID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST,
+    ADD UNIQUE KEY `GruppeKosten` (`Gruppe`, `Kostenstelle`);
